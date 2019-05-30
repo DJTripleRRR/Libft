0cd3dbf150e1e8b587990885c855eb5fe11d7d56
Ryan SMITH <rysmith@c4r2s5.wethinkcode.co.za>
2019-05-30 10:47:19 +0200

    Adding exception for NULL returns

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 17:05:51 by rysmith           #+#    #+#             */
-/*   Updated: 2019/05/29 17:13:21 by rysmith          ###   ########.fr       */
+/*   Updated: 2019/05/30 10:45:50 by rysmith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 void	*ft_memccpy(void *str1, const void *str2, int c, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (n > 0 && ((char *)str2)[i] != (unsigned char)c)
+	if (n = 0)
+	{
+		return (NULL);
+	}
+	while (n > 0 ||	((unsigned char *)str2)[i] != (unsigned char)c)
 	{
 		((char *)str1)[i] = ((char *)str2)[i];
 		i++;
