f827e19710ad11850bbe4953341a9e5f223493f5
Ryan SMITH <rysmith@c4r9s6.wethinkcode.co.za>
2019-06-12 16:08:09 +0200

    Adding intial ft_strstr.c (untested)

---
+++ ft_strstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/12 15:37:25 by rysmith           #+#    #+#             */
+/*   Updated: 2019/06/12 16:07:21 by rysmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (needle[0] = '\0')
+	{
+		return (haystack[0]);
+	}
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		if (haystack[i] = needle[j])
+		{
+			while (needle[j] == haystack[i + j])
+			{
+				if (needle[j + i] == '\0')
+				{
+					return ((char *)haystack + i);
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
