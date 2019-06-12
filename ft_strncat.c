25e0d312058b7c47a62de4298984b9adeb8b4059
Ryan SMITH <rysmith@c4r9s6.wethinkcode.co.za>
2019-06-12 14:46:38 +0200

    Adding initial ft_strncat.c (Untested)

---
+++ ft_strncat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/12 14:42:43 by rysmith           #+#    #+#             */
+/*   Updated: 2019/06/12 14:46:00 by rysmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <string.h>
+
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
+{
+	size_t i;
+	size_t j;
+
+	i = ft_strlen(s1);
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		s1[i] = s2[j];
+		j++;
+		i++;
+	}
+	while (j > n)
+	{
+		s1[i] = '\0';
+		i--;
+		j--;
+	}
+	s1[i] = s2[j];
+	return (s1);
+}
