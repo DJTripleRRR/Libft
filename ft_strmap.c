2687c0fe3a93c1bd9f619d58e17911277a006caf
Ryan SMITH <rysmith@c4r8s9.wethinkcode.co.za>
2019-06-27 13:08:03 +0200

    Addining initial ft_strmap.c (untested)

---
+++ ft_strmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/27 12:57:07 by rysmith           #+#    #+#             */
+/*   Updated: 2019/06/27 13:06:55 by rysmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <libft.h>
+
+char * ft_strmap(char const *s, char (*f)(char))
+{
+	size_t	i;
+	char	*mem;
+
+	i = 0;
+	if (s && f)
+	{
+		if (!(mem = (char*)malloc(sizeof (char) * (ft_strlen(s) = 1))))
+		{
+			return (NULL);
+		}
+		while (s[i] != '\0')
+		{
+			mem[i] = s[i];
+			mem[i] = f(mem[i]);
+			i++
+		}
+		mem[i] = '\0';
+	}
+	return (mem);
+}
