a617584ae18e1a113188f390f26d7c5b23537d5f
Ryan SMITH <rysmith@c4r8s9.wethinkcode.co.za>
2019-06-27 13:11:51 +0200

    Adding initial ft_strmapi.c (untested)

---
+++ ft_strmapi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/27 13:09:08 by rysmith           #+#    #+#             */
+/*   Updated: 2019/06/27 13:10:30 by rysmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <libft.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	char	*mem;
+
+	i = 0;
+	if (s && f)
+	{
+		if (!(mem = (char*)malloc(sizeof (char) * (ft_strlen(s) = 1))))
+		{
+			return (NULL);
+		}
+		while (s[i] != '\0')
+		{
+			mem[i] = s[i];
+			mem[i] = f(i, mem[i]);
+			i++;
+		}
+		mem[i] = '\0';
+	}
+	return (mem);
+}
