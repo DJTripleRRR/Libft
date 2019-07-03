4469a860f05131da51e2fd52e5f1ed816eb172e0
Ryan SMITH <rysmith@c4r8s9.wethinkcode.co.za>
2019-07-03 13:14:39 +0200

    Adding inital ft_strsplit.c (untested)

---
+++ ft_strsplit.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysmith <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/27 14:48:13 by rysmith           #+#    #+#             */
+/*   Updated: 2019/07/03 13:13:39 by rysmith          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <libft.h>
+#include <libft.h>
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char			**mem;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	k;
+
+	i = 0;
+	k = 0;
+	if (!(s && c))
+		return (NULL);
+	if (!(mem = (char **)malloc(sizeof(char*) * (ft_cntchr(s, c) + 1))))
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			j = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			mem[k] = ft_strsub(s, j, (i - j));
+			k++;
+		}
+	}
+	return (mem);
+}
